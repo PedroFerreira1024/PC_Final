1ff9578994517b36f6d874b779644e5420bace58
PedroFerreira1024 <pedroferreira1024@gmail.com>
2014-02-20 14:21:05 +0000

    Ex3 concluido

--- PC_Final/Ex3/Program.cs
+++ PC_Final/Ex3/Program.cs
@@ -10,6 +10,8 @@ namespace Ex3
 {
     public class Program
     {
+        public static CancellationTokenSource cToken;
+
         public class SearchObj
         {
             public volatile int countTotal;
@@ -22,21 +24,27 @@ namespace Ex3
             }
         }
 
-        public static Task<SearchObj> searchAndListFiles(String rootPath, String extension, String toLookFor)
+        public static Task<SearchObj> searchAndListFiles(String rootPath, String extension, String toLookFor, CancellationToken token)
         {
+            token.ThrowIfCancellationRequested();
             DirectoryInfo root = new DirectoryInfo(rootPath);
 
             var rootTasks = new List<Task<SearchObj>>();
 
             foreach(var dir in root.EnumerateDirectories())
             {
-                rootTasks.Add(searchAndListFiles(rootPath + "\\" + dir.Name, extension, toLookFor));
+                rootTasks.Add(searchAndListFiles(rootPath + "\\" + dir.Name, extension, toLookFor,token));
             }
+
+            //
+            //Para testar cancelamento
+            //cToken.Cancel();
 
             var tasks = new List<Task<SearchObj>>();
             var obj = new SearchObj();
             foreach(var file in root.EnumerateFiles())
             {
+                token.ThrowIfCancellationRequested();
                 Interlocked.Increment(ref obj.countTotal);
                 if (file.Extension.Equals(extension))
                 {
@@ -48,6 +56,7 @@ namespace Ex3
                         {
                             byte[] buff = new byte[file.Length];
                             UTF8Encoding temp = new UTF8Encoding(true);
+
                             while (stream.Read(buff, 0, buff.Length) > 0)
                             {
                                 if (temp.GetString(buff).Contains(toLookFor))
@@ -58,9 +67,12 @@ namespace Ex3
                             }
                             return obj;
                         }
-                    }));
+                    },token));
+                    
                 }
             }
+
+            token.ThrowIfCancellationRequested();
 
             return Task.WhenAll(rootTasks).ContinueWith<SearchObj>((x) => {
                 
@@ -77,8 +89,19 @@ namespace Ex3
 
         public static void Main(string[] args)
         {
-            var xpto = searchAndListFiles(@"C:\Users\Pedro\Desktop\PC", ".txt", "PC").Result;
+            cToken = new CancellationTokenSource();
+            
+            try
+            {
+                var xpto = searchAndListFiles(@"C:\Users\Pedro\Desktop\PC", ".txt", "PC", cToken.Token).Result;
+            }
+            catch(OperationCanceledException)
+            {
+                Console.WriteLine("Cancelado");
+            }
+            
             Console.Read();
+                       
         }
     }
 }
